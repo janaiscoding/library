3e148de96928abc648f958f8d062984c862ac91a
JanaIsCoding <jana.istrate@gmail.com>
2022-11-26 17:02:14 +0200

    Add, clear, show books/library functions/buttons

--- javascript.js
+++ javascript.js
@@ -12,11 +12,39 @@ function addBookToLibrary(givenTitle,givenAuthor) {
    myLibrary.push(givenBook);
   }
 
-//testing books 
-addBookToLibrary('a','b')
-addBookToLibrary('c','d')
-addBookToLibrary('e','f')
-
+//loop and display everything that was given
+function displayBooks(){
 for (const book of myLibrary){
     console.table(book);
 }
+}
+
+//refresh user inputs 
+function clearInput(){
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+}
+
+// store user inputs and add them to the library
+const addBookBtn = document.querySelector('.add-book');
+addBookBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const givenTitle = document.querySelector('#title').value;
+    const givenAuthor = document.querySelector('#author').value;
+    addBookToLibrary(givenTitle,givenAuthor);
+    clearInput();
+})
+
+// clean inputs button
+const clearBtn = document.querySelector('.clear-inputs');
+clearBtn.addEventListener('click', (e) => { 
+    e.preventDefault();
+    clearInput();
+});
+
+// display all the books
+const showLibrary = document.querySelector('.show-library');
+showLibrary.addEventListener ('click', (e) => { 
+    e.preventDefault();
+    displayBooks();
+});
