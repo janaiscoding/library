3bed23baf660f2fc3798aeb2c781d706feedb593
JanaIsCoding <jana.istrate@gmail.com>
2022-11-26 18:22:19 +0200

    display books in a table

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 let myLibrary = [];
-
+let headers = ["title", "author"];
 //constructor
 function Book(title, author){
     this.title = title;
@@ -13,16 +13,38 @@ function addBookToLibrary(givenTitle,givenAuthor) {
   }
 
 //loop and display everything that was given
-function displayBooks(){
-for (const book of myLibrary){
-    console.table(book);
-}
-}
+let box = document.getElementById("box");
+function displayLibrary(){
+// Get rid of the prior table (if it exists)
+if(document.getElementById("libraryTable")){
+    document.getElementById("libraryTable").remove();
+  }
 
-//refresh user inputs 
-function clearInput(){
-    document.querySelector('#title').value = '';
-    document.querySelector('#author').value = '';
+  let tbl = document.createElement("table");
+  tbl.id = "libraryTable";
+
+  for (i = 0; i < myLibrary.length; i++) {
+    let row = document.createElement("tr");
+    //looping the headers' elements to create all the cells
+    headers.forEach(function(header, index){
+        let cell = document.createElement("td");
+        cell.textContent = myLibrary[i][header];
+        cell.classList.add("data" + (index + 1));
+        row.appendChild(cell); 
+    });
+      
+    let deleteBtn = document.createElement("td");
+        deleteBtn.textContent = "x";
+        deleteBtn.classList.add("delete");
+    
+        deleteBtn.addEventListener("click", function(){
+        myLibrary.splice(i, 1);  
+        this.closest("tr").remove(); 
+    });
+      row.appendChild(deleteBtn); 
+      tbl.appendChild(row);     
+      box.appendChild(tbl);     
+    }
 }
 
 // store user inputs and add them to the library
@@ -33,6 +55,7 @@ addBookBtn.addEventListener('click', (e) => {
     const givenAuthor = document.querySelector('#author').value;
     addBookToLibrary(givenTitle,givenAuthor);
     clearInput();
+    displayLibrary();
 })
 
 // clean inputs button
@@ -42,9 +65,8 @@ clearBtn.addEventListener('click', (e) => {
     clearInput();
 });
 
-// display all the books
-const showLibrary = document.querySelector('.show-library');
-showLibrary.addEventListener ('click', (e) => { 
-    e.preventDefault();
-    displayBooks();
-});
+//refresh user inputs 
+function clearInput(){
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+}
