dc615252f11c68d8aba97f9e483ee5bb91721284
JanaIsCoding <jana.istrate@gmail.com>
2023-02-24 21:07:03 +0200

    Add email validation

--- validation.js
+++ validation.js
@@ -1,29 +1,8 @@
-window.onload = () => {
-    document.getElementById('email').onchange = validateEmail;
-}
-
-const email = document.getElementById('email')
 const country = document.getElementById('country')
 const zipcode = document.getElementById('zip-code')
 const password = document.getElementById('password')
 const passwordConfirm = document.getElementById('password-confirm')
 const submitButton = document.getElementById('submit')
-
-const validateEmail = function(){
-    const email = document.getElementById('email')
-    const patternEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(email.value.match(pattern)){
-        alert("Valid")
-        return true;
-        
-    }
-    else {
-        alert("Wrong")
-        email.classList.add('email:invalid')
-        messages.emailError()
-        return false
-    }
-}
 
 const submitEvent = function(e){
     e.preventDefault()
@@ -31,15 +10,27 @@ const submitEvent = function(e){
 }
 submitButton.onclick = submitEvent;
 
-let messages = new Messages()
-
-class Messages(){
-    constructor(error,success){
-        this.error = []
-        this.success = []
-    }
-    emailError(){
+class handleForm {
+    static emailError(){
+        alert("Wrong")
         this.error.push('Email is incorrect')
         alert('Email is incorrect')
+        email.classList.add('email:invalid')
     }
+    static emailSuccess(){
+        alert("Valid")
+        email.classList.add('email:valid')
+    }
+
+    static validateEmail(){
+    const email = document.getElementById('email')
+    const patternEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(email.value.match(patternEmail)) handleForm.emailSuccess()
+    else handleForm.emailError()
+    }
+}
+
+let validation = new handleForm()
+window.onload = () => {
+    document.getElementById('email').onchange = validation.validateEmail;
 }
