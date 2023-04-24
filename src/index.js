e3c5c45c70192e10fdcf66e605d7839089c0e9af
janaiscoding <jana.istrate@gmail.com>
2023-04-24 23:20:46 +0300

    Add delete book, book status and each event listener on them

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ import {
   updateDoc,
   doc,
   serverTimestamp,
+  deleteDoc,
 } from "firebase/firestore";
 import {
   getStorage,
@@ -131,7 +132,8 @@ const onBookFormSubmit = (e) => {
     saveBook(
       authorInputElement.value,
       titleInputElement.value,
-      pagesInputElement.value
+      pagesInputElement.value,
+      checkboxInputElement.checked
     ).then(() => {
       bookFormElement.reset();
       modalElement.style.display = "none";
@@ -139,8 +141,8 @@ const onBookFormSubmit = (e) => {
   }
 };
 
-// 2.2 Sends book to the collection
-async function saveBook(author, title, pages) {
+// 2.2 Sends book to the collection from the frontend
+async function saveBook(author, title, pages, isRead) {
   // Add a new book entry to the Firebase database.
   try {
     await addDoc(collection(getFirestore(), "books"), {
@@ -148,13 +150,14 @@ async function saveBook(author, title, pages) {
       title: title,
       pages: pages,
       timestamp: serverTimestamp(),
+      isRead: isRead,
     });
   } catch (error) {
     console.error("Error writing new book to Firebase Database", error);
   }
 }
 
-// Loads books and listens for upcoming ones.
+// 2.3 Loads books and listens for upcoming ones on the UI.
 function loadBooks() {
   // Create the query to load the last 6 books and listen for new ones.
   const recentBooksQuery = query(
@@ -167,8 +170,7 @@ function loadBooks() {
   onSnapshot(recentBooksQuery, function (snapshot) {
     snapshot.docChanges().forEach(function (change) {
       if (change.type === "removed") {
-        // deleteBook(change.doc.id);
-        console.log("this what happens when i delete book");
+        deleteBook(change.doc.id);
       } else {
         const book = change.doc.data();
         displayBook(
@@ -176,13 +178,16 @@ function loadBooks() {
           book.author,
           book.title,
           book.pages,
+          book.isRead,
           book.timestamp
         );
       }
     });
   });
 }
-const displayBook = (id, author, title, pages, timestamp) => {
+
+// 2.4 Displays the book elements and the event listeners
+const displayBook = (id, author, title, pages, isRead, timestamp) => {
   //Editing an existing book based on book id || Creating a new book
   const bookDiv =
     document.getElementById(id) || createAndInsertBook(id, timestamp);
@@ -190,28 +195,65 @@ const displayBook = (id, author, title, pages, timestamp) => {
   const bookAuthor = bookDiv.querySelector(".book-author");
   const bookTitle = bookDiv.querySelector(".book-title");
   const bookPages = bookDiv.querySelector(".book-pages");
-  const addedAt = bookDiv.querySelector(".book-timestamp");
+  const removeButton = bookDiv.querySelector(".remove-book");
+  const statusButton = bookDiv.querySelector(".status");
 
   bookAuthor.textContent = `Written by ${author}`;
   bookTitle.textContent = title;
   bookPages.textContent = `${pages} pages`;
-  addedAt.textContent = `Added at ${timestamp.toDate().toTimeString()}`;
+
+  // initial status
+  if (isRead) {
+    statusButton.textContent = "Read";
+    bookDiv.classList.remove("status-unread");
+    bookDiv.classList.add("status-read");
+  } else {
+    statusButton.textContent = "Not Read";
+    bookDiv.classList.remove("status-read");
+    bookDiv.classList.add("status-unread");
+  }
+
   // to add : remove and edit button
+  removeButton.addEventListener("click", () => {
+    deleteBook(id);
+  });
+  statusButton.addEventListener("click", () => {
+    let isReadUpdated = !isRead;
+    changeBookStatus(id, isReadUpdated);
+  });
 };
 
-// Template for messages.
+// 2.5 Function for deleting a certain book
+const deleteBook = (id) => {
+  const bookDiv = document.getElementById(id);
+  const db = getFirestore();
+  const docRef = doc(db, "books", id);
+  deleteDoc(docRef);
+  if (bookDiv) {
+    bookDiv.parentNode.removeChild(bookDiv);
+  }
+};
+// 2.6 Change a book's read status
+const changeBookStatus = (id, isRead) => {
+  const db = getFirestore();
+  const docRef = doc(db, "books", id);
+  updateDoc(docRef, {
+    isRead: isRead,
+  });
+};
+// 2.6 Template for book's html.
 const BOOK_TEMPLATE =
   '<div class="book">' +
   '<div class="book-title"></div>' +
   '<div class="book-author"></div>' +
   '<div class="book-pages"></div>' +
-  '<div class="book-timestamp"></div>' +
-  '<div class="book-buttons-wrapper">'+
-  '<button class="status">Read/Not Read</button>' +
+  '<div class="book-buttons-wrapper">' +
+  '<button class="status"></button>' +
   '<button class="remove-book">Remove</button>' +
-  '</div>' +
+  "</div>" +
   "</div>";
 
+// 2.7  Creates the div and pushes them to the DOM
 const createAndInsertBook = (id, timestamp) => {
   const container = document.createElement("div");
   container.innerHTML = BOOK_TEMPLATE;
@@ -228,29 +270,30 @@ const createAndInsertBook = (id, timestamp) => {
   if (existingBooks.length === 0) {
     librarySpaceElement.appendChild(div); //if there's none, just push it in
   } else {
-    let messageListNode = existingBooks[0];
+    let bookListNode = existingBooks[0];
 
-    while (messageListNode) {
-      const messageListNodeTime = messageListNode.getAttribute("timestamp");
+    while (bookListNode) {
+      const bookListNodeTime = bookListNode.getAttribute("timestamp");
 
-      if (!messageListNodeTime) {
+      if (!bookListNodeTime) {
         throw new Error(
-          `Child ${messageListNode.id} has no 'timestamp' attribute`
+          `Child ${bookListNode.id} has no 'timestamp' attribute`
         );
       }
 
-      if (messageListNodeTime < timestamp) {
+      if (bookListNodeTime < timestamp) {
         break;
       }
 
-      messageListNode = messageListNode.nextSibling;
+      bookListNode = bookListNode.nextSibling;
     }
 
-    librarySpaceElement.insertBefore(div, messageListNode);
+    librarySpaceElement.insertBefore(div, bookListNode);
   }
 
   return div;
 };
+
 // Shortcuts to DOM Elements.
 const signInButtonElement = document.getElementById("sign-in");
 const signOutButtonElement = document.getElementById("sign-out");
@@ -261,6 +304,7 @@ const authInfoPanel = document.querySelector(".auth-info-panel");
 const authorInputElement = document.getElementById("author");
 const titleInputElement = document.getElementById("title");
 const pagesInputElement = document.getElementById("pages");
+const checkboxInputElement = document.getElementById("status");
 const bookFormElement = document.getElementById("book-form");
 const openModalElement = document.getElementById("open-modal");
 const closeModalElement = document.getElementById("close-modal");
@@ -271,6 +315,7 @@ const librarySpaceElement = document.getElementById("library-space");
 signOutButtonElement.addEventListener("click", signOutUser);
 signInButtonElement.addEventListener("click", signIn);
 bookFormElement.addEventListener("submit", onBookFormSubmit);
+
 openModalElement.onclick = () => {
   modalElement.style.display = "block";
 };
